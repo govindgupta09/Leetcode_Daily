536941cd39d7b74238fe064b557e1ef849f0c0f6
Govind Gupta <95240802+govindgupta09@users.noreply.github.com>
2026-07-05 16:54:05 +0530

    Time: 14 ms (58.82%), Space: 47.1 MB (22.35%) - LeetHub

---
+++ 1301-number-of-paths-with-max-score/1301-number-of-paths-with-max-score.java
@@ -0,0 +1,65 @@
+class Solution {
+    private static final int MOD = 1_000_000_007;
+
+    public int[] pathsWithMaxScore(List<String> board) {
+        int n = board.size();
+
+        int[][] score = new int[n][n];
+        int[][] ways = new int[n][n];
+
+        for (int[] row : score) {
+            Arrays.fill(row, -1);
+        }
+
+        score[n - 1][n - 1] = 0;
+        ways[n - 1][n - 1] = 1;
+
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = n - 1; j >= 0; j--) {
+
+                if (board.get(i).charAt(j) == 'X')
+                    continue;
+
+                if (i == n - 1 && j == n - 1)
+                    continue;
+
+                int maxScore = -1;
+                int count = 0;
+
+                int[][] dirs = {{1, 0}, {0, 1}, {1, 1}};
+
+                for (int[] d : dirs) {
+                    int ni = i + d[0];
+                    int nj = j + d[1];
+
+                    if (ni >= n || nj >= n)
+                        continue;
+
+                    if (score[ni][nj] == -1)
+                        continue;
+
+                    if (score[ni][nj] > maxScore) {
+                        maxScore = score[ni][nj];
+                        count = ways[ni][nj];
+                    } else if (score[ni][nj] == maxScore) {
+                        count = (count + ways[ni][nj]) % MOD;
+                    }
+                }
+
+                if (maxScore == -1)
+                    continue;
+
+                char c = board.get(i).charAt(j);
+                int val = (c == 'E') ? 0 : c - '0';
+
+                score[i][j] = maxScore + val;
+                ways[i][j] = count;
+            }
+        }
+
+        if (ways[0][0] == 0)
+            return new int[]{0, 0};
+
+        return new int[]{score[0][0], ways[0][0]};
+    }
+}
