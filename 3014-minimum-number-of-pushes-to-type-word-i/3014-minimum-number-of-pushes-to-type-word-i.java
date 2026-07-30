b510fff7ff3b94ab3d487c865bb0c5d101647224
Govind Gupta <95240802+govindgupta09@users.noreply.github.com>
2026-07-30 17:23:11 +0530

    Time: 0 ms (100%), Space: 42.9 MB (95.69%) - LeetHub

---
+++ 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int minimumPushes(String word) {
+        int len = word.length();
+        int res = 0;
+        if(len<=8){
+            res = len;
+        }else if(len>8 && len<17){
+            res = 8+2*(len-8);
+        }else if(len>16 && len<25){
+            res = 24+3*(len-16);
+        }else if(len>24 && len<33){
+            res = 48+4*(len-24);
+        }else{
+            res = 80+5*(len-32);
+        }
+        return res;
+    }
+}
