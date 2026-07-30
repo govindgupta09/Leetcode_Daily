48ded695646ace21b06be2e661b6c23eea7bdfde
Govind Gupta <95240802+govindgupta09@users.noreply.github.com>
2026-07-30 17:25:01 +0530

    Time: 0 ms (100%), Space: 43.3 MB (47.13%) - LeetHub

--- 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.java
+++ 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.java
@@ -8,10 +8,8 @@ class Solution {
             res = 8+2*(len-8);
         }else if(len>16 && len<25){
             res = 24+3*(len-16);
-        }else if(len>24 && len<33){
-            res = 48+4*(len-24);
         }else{
-            res = 80+5*(len-32);
+            res = 48+4*(len-24);
         }
         return res;
     }
