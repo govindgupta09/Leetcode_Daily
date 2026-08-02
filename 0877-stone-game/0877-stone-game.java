b14cf8df338e4d79f18672a6bae1264bcb6865e7
Govind Gupta <95240802+govindgupta09@users.noreply.github.com>
2026-08-02 21:11:20 +0530

    Time: 0 ms (100%), Space: 42.6 MB (97.64%) - LeetHub

--- 0877-stone-game/0877-stone-game.java
+++ 0877-stone-game/0877-stone-game.java
@@ -16,14 +16,14 @@ class Solution {
         
     }
 
-    private int solve(int i, int j, int[] piles){
+    // private int solve(int i, int j, int[] piles){
 
-        if(i>j) return 0;
-        if(i==j) return piles[i];
+    //     if(i>j) return 0;
+    //     if(i==j) return piles[i];
 
-        int item_i = piles[i] + Math.min(solve(i+2, j, piles), solve(i+1, j-1, piles));
-        int item_j = piles[j] + Math.min(solve(i+1, j-1, piles), solve(i, j-2, piles));
+    //     int item_i = piles[i] + Math.min(solve(i+2, j, piles), solve(i+1, j-1, piles));
+    //     int item_j = piles[j] + Math.min(solve(i+1, j-1, piles), solve(i, j-2, piles));
 
-        return Math.max(item_i, item_j);
-    }
+    //     return Math.max(item_i, item_j);
+    // }
 }
