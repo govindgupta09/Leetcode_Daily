7c3375b2315476fa896e9609d05e570519ac7678
Govind Gupta <95240802+govindgupta09@users.noreply.github.com>
2026-08-06 22:28:32 +0530

    Time: 1 ms (100%), Space: 42.8 MB (30.89%) - LeetHub

--- 3345-smallest-divisible-digit-product-i/3345-smallest-divisible-digit-product-i.java
+++ 3345-smallest-divisible-digit-product-i/3345-smallest-divisible-digit-product-i.java
@@ -7,12 +7,9 @@ class Solution {
             int num = res;
             while(num>0){
             int digit = num%10;
-            System.out.println("digit= "+digit);
             val*=digit;
-            System.out.println("val= "+val);
             num/=10;
             }
-            System.out.println("val= "+val+", res= "+res);
             if(val%t == 0){
                 bool = false;
             }else{
